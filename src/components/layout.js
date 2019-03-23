ff2ace2876c3c95f5c8f339252f63fbe1d11f7c7
rjwu95 <yugun000@gmail.com>
2019-03-23 22:18:56 +0900

    Test deploy 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ children }) => (
         <h3 style={{ display: "inline" }}>gun's blog</h3>
       </Link>
       <ul style={{ listStyle: "none", float: "right" }}>
-        <ListLink to="/">Home</ListLink>
+        {/* <ListLink to="/">Home</ListLink>
         <ListLink to="/about">About</ListLink>
-        {/* <ListLink to="/contact">Contact</ListLink> */}
+        <ListLink to="/contact">Contact</ListLink> */}
       </ul>
     </header>
     {children}
